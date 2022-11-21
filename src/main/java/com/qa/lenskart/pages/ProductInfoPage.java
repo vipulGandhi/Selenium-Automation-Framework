ec7f2482dce68fda9e6bce6f8e466985312b2625
Vipul Gandhi <vipulgandhi10@gmail.com>
2022-11-21 18:17:37 +0530

    Commit
    Commit

--- src/main/java/com/qa/lenskart/pages/ProductInfoPage.java
+++ src/main/java/com/qa/lenskart/pages/ProductInfoPage.java
@@ -3,9 +3,13 @@
  */
 package com.qa.lenskart.pages;
 
+import java.util.HashMap;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import com.qa.lenskart.utils.Constants;
 import com.qa.lenskart.utils.ElementUtil;
 import com.qa.lenskart.utils.JavascriptUtil;
 import com.qa.lenskart.utils.PageUtils;
@@ -15,22 +19,43 @@ public class ProductInfoPage
 {
 	private ElementUtil elementUtil;
 	private PageUtils pageUtils;
+	private WaitUtils waitUtils;
 	
 	private String pageHeading;
+	private HashMap<String, String> productTechnicalInfoHashMap;
 	
 	By pageHeadingBy = By.xpath("//div[contains(@class,'product-brands')]");
+	By productImageCountBy = By.xpath("//div[@class='product-images-list']//li");
+	By technicalInfoButtonBy = By.xpath("//span[text()='Technical Information']");
+	By productTechnicalInfoMetadataBy = By.xpath("//span[text()='Technical Information']/../../following-sibling::div//span[contains(@class,'info-text')]");
 	
 	public ProductInfoPage(WebDriver driver)
 	{
 		elementUtil = new ElementUtil(driver);
 		pageUtils = new PageUtils(driver);
+		waitUtils = new WaitUtils(driver);
 	}
 	
 	public String getPageHeading()
 	{
 		pageHeading = elementUtil.getElementText(pageHeadingBy);
 		pageUtils.switchToParentWindow();
-		return pageHeading;
-		
+		return pageHeading;	
+	}
+	
+	public int productImagesCount()
+	{
+		return elementUtil.getElementsCount(waitUtils.isVisibilityOfElementsLocated(productImageCountBy, Constants.DEFAULT_TIME_OUT));
+	}
+	
+	public HashMap<String, String> getProductTechnicalInfo()
+	{
+		elementUtil.doClick(technicalInfoButtonBy);
+		List<String> productTechnicalMetadataList = 
+				elementUtil.getElementsText(waitUtils.isVisibilityOfElementsLocated(productTechnicalInfoMetadataBy, Constants.DEFAULT_TIME_OUT));
+		for (int i = 0; i < productTechnicalMetadataList.size(); i++)
+		{
+			
+		}
 	}
 }
