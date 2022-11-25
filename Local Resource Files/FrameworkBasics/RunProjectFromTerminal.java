88d865a8f8ecefd1ff18236bd371b4d692f490a6
Vipul Gandhi <vipulgandhi10@gmail.com>
2022-11-25 17:25:00 +0530

    Commit
    Commit

--- Local Resource Files/FrameworkBasics/RunProjectFromTerminal.java
+++ Local Resource Files/FrameworkBasics/RunProjectFromTerminal.java
@@ -6,15 +6,16 @@
 		// echo $JAVA_HOME
 		// https://www.youtube.com/watch?v=Kq3ppMu2iuk
 		// https://www.youtube.com/watch?v=hRThN2gal6U&t=390s
-	// 4. Install Maven
-		// 4.1	Install Homebrew
-		// 4.2	The command "brew install maven" will install the latest Maven and will set MAVEN_HOME path
+	// 4. Install homebrew
+		// https://stackoverflow.com/questions/66666134/how-to-install-homebrew-on-m1-mac
+	// 5. Install Maven
+		// The command "brew install maven" will install the latest Maven and will set MAVEN_HOME path
 		// https://mkyong.com/maven/install-maven-on-mac-osx/
-	//	5. Add testng.xml configuration in surefire plugin in pom.xml file
-	//	6. Open Terminal
+	// 5. Add testng.xml configuration in surefire plugin in pom.xml file
+	// 6. Open Terminal
 		// Go to the project path where pom.xml is present
 			// cd /Users/vipulgandhi/Documents/Vipul/Automation/Projects/Automation_Framework
-	//	7. Run command "mvn clean install -Denvironment="qa""
+	// 7. Run command "mvn clean install -Denvironment="qa""
 		// -Denvironment="qa" = -Dkey="value"
 			// We have set the key-value pairs in DriverFactory class
 				// Key = "environment" [ System.getProperty("environment").trim().toLowerCase();]
