4093716bc9a7b28d4469ccd17129891a04568ec9
Vipul Gandhi <vipulgandhi10@gmail.com>
2022-11-26 14:37:22 +0530

    Commit
    Commit

--- Local Resource Files/FrameworkBasics/GitCommands.java
+++ Local Resource Files/FrameworkBasics/GitCommands.java
@@ -1,5 +1,4 @@
-// Login to git
-// Create a repo in git
+// Login to git, create a repo
 // Install git on local machine
 	// brew install git
 //	Verify if git is installed
@@ -26,6 +25,26 @@
 	// Check status of local/ working copy (how many files are pending/ pushed/ committed)
 		// git status
 			// Number of files to commit will be reduced
+	// Add all the untracked files to git stage
+		// git add .
+			// Create a snapshot of the working copy, add it to git stage
+	// Check status of local/ working copy (how many files are pending/ pushed/ committed)
+		// git status
+			// All files are ready to be committed now
+	// Commit
+		// git commit -m "Commit"
+			// -m is the commit message
+		// Files are now reached to local repository
+	// Push
+		// git push origin master OR git push origin main
+			// master/ main is the name of root directory
+			// 1st commit goes to master branch
+		// Enter username: vipulgandhi10@gmail.com
+		// Enter password/ passphrase
+			// 
+
+
+
 
 
 
