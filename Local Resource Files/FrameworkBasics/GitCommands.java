5e8b886a6cdd32cc6480fa92fa319f8ef81efe78
Vipul Gandhi <vipulgandhi10@gmail.com>
2022-11-26 13:27:58 +0530

    Commit
    Commit

---
+++ Local Resource Files/FrameworkBasics/GitCommands.java
@@ -0,0 +1,13 @@
+// Install git
+	// brew install git
+//	Verify if git is installed
+	// git --version
+// Open terminal
+	// Go to project's present working directory
+		// cd /Users/vipulgandhi/Documents/Vipul/Automation/Projects/Automation_Framework
+	// Initialize working copy with git
+		// git init
+			// .git directory(hidden) will be created in the project 
+	// Check if the .git directory is created inside the project
+		// ls -alt
+			// It displays all the files present in present working directory
