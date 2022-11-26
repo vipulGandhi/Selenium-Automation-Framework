1852d05d1d340e9eb7d6baf923115fb7284d80a1
Vipul Gandhi <vipulgandhi10@gmail.com>
2022-11-26 13:55:05 +0530

    Commit
    Commit

--- Local Resource Files/FrameworkBasics/GitCommands.java
+++ Local Resource Files/FrameworkBasics/GitCommands.java
@@ -1,4 +1,6 @@
-// Install git
+// Login to git
+// Create a repo in git
+// Install git on local machine
 	// brew install git
 //	Verify if git is installed
 	// git --version
@@ -11,3 +13,15 @@
 	// Check if the .git directory is created inside the project
 		// ls -alt
 			// It displays all the files present in present working directory
+	// Connect local project to repo
+		// git remote add origin https://github.com/vipulGandhi/Selenium_Automation_Framework.git
+	// Check status of local/ working copy (how many files are pending/ pushed/ committed)
+		// git status
+// Add .gitignore file in the project
+	// Git ignores the files present in .gitignore to upload them to the repository
+	// Go to project
+	// Right click -> Create a new file with name: .gitignore
+
+
+// Untracked files: Files which still needs to be committed
+	
