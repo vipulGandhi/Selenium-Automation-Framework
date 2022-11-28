21a99aa16ebe1f6f1c30b5cd22c90ad4058b81a4
Vipul Gandhi <vipulgandhi10@gmail.com>
2022-11-28 10:28:30 +0530

    Commit
    Commit

---
+++ LocalResourceFiles/Framework Basics/Jenkins/JenkinsSetup.java
@@ -0,0 +1,19 @@
+// Setup Jenkins server on your machine
+	// Download LTS jenkins war file
+		// https://get.jenkins.io/war-stable/
+	// Execute jenkins.war from terminal
+		// Open terminal
+			// Go to the directory where jenkins.war file is located
+				// cd /Users/vipulgandhi/Documents/Vipul/Softwares
+			// Execute: java -jar jenkins.war
+			// Copy the password displayed in the terminal
+				// The password is also stored at /Users/vipulgandhi/.jenkins/secrets/initialAdminPassword
+			// Open a new tab in any browser
+				// Enter: localhost:8080
+				// Default port number for jenkins is 8080
+				// Jenkins configuration setup will start
+					// Unlock Jenkins page: Enter the password
+					// Customize Jenkins page: Click "Install suggested plugins" 
+						// Jenkins server is started
+						// Data is getting downloaded, progress is displayed on UI and in terminal [Do not close terminal]
+
--- LocalResourceFiles/Framework Basics/Jenkins/Jenkins_Basics.java
+++ LocalResourceFiles/Framework Basics/Jenkins/Jenkins_Basics.java
@@ -3,6 +3,22 @@
 	// Browsers are launched in local with sequential/ parallel execution
 // Push the code in git repo in main branch
 // Setup a jenkins server to trigger the test cases
+	// Create a job
+		// Job configurations
+			// Parameterize the job
+			// Scheduling/ polling of the job
+			// Configure maven and execute maven test cases
+			// Integrate with git
+				// Setup auto trigger feature
+					// Moment the code is pushed in git, it will automatically be triggered for execution
+	// When the job is triggered
+		// Jenkins will integrate with the git repo branch
+		// It will pick the code from git to local and create a workspace (clone the project)
+		// The browser will be launched on jenkins machine
+			// Not a good practice
+				// All the teams/ team members are using common jenkins server
+					// The browser(when triggered) needs to finish 1st task before jumping to the next one
+			// Setup the execution environment on a different machine or on cloud [Let the browser be launched there]
 
 // Jenkins
 	// Jenkins is a CI/CD tool
@@ -14,14 +30,6 @@
 		// With the given ip address and port number, they can start running the test cases
 	// It is Dev-Ops team's responsibility to setup the jenkins
 
-	// Create a job
-		// Job configurations
-			// Parameterize the job
-			// Scheduling/ polling of the job
-			// Configure maven and execute maven test cases
-			// Integrate with git
-				// Setup auto trigger feature
-					// Moment the code is pushed in git, it will automatically be triggered for execution
 
 	// Create a declarative pipeline
 		// Create jenkins file
